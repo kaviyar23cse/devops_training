b447f7a759bcee3e4c239c0e2005eac63e6eac13
kaviyar23cse <kaviyar.23cse@kongu.edu>
2026-02-14 13:40:51 +0530

    changed workflow

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -39,7 +39,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
--- eks-terraform/variables.tf
+++ eks-terraform/variables.tf
@@ -3,5 +3,5 @@ variable "region" {
 }
 
 variable "cluster_name" {
-  default = ""
+  default = "kaviya-eks-cluster"
 }
